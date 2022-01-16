8269486022920ff8ad2ad2377d2503ce8ce5b128
Christi <christi.tahk@gmail.com>
2022-01-15 18:36:47 -0600

    Initial commit

---
+++ src/main/java/com/example/believeinyourshelves/BelieveInYourShelvesApplication.java
@@ -0,0 +1,13 @@
+package com.example.believeinyourshelves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelieveInYourShelvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelieveInYourShelvesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/believeinyourshelves/BelieveInYourShelvesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.believeinyourshelves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelieveInYourShelvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
