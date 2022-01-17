ae427f12e7e28f21e1e189e707712f54c42d36c0
Christi <christi.tahk@gmail.com>
2022-01-16 21:13:34 -0600

    added models and additional templates

---
+++ src/main/java/com/example/believeinyourshelves/models/Book.java
@@ -0,0 +1,39 @@
+package com.example.believeinyourshelves.models;
+
+public class Book {
+
+    private String name;
+
+    private String author;
+
+    public String getName() {
+        return name;
+    }
+
+    public Book() {}
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
