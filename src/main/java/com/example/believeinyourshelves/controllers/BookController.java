64adfef8824272b419792736583c34aa7d16c837
Christi <christi.tahk@gmail.com>
2022-01-16 22:25:16 -0600

    book index

---
+++ src/main/java/com/example/believeinyourshelves/controllers/BookController.java
@@ -0,0 +1,9 @@
+package com.example.believeinyourshelves.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("books")
+public class BookController {
+}
